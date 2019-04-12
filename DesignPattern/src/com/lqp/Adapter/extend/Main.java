459d29912403cf66e219c4f00adc18567ae880bd
lqpNISL <707901160@qq.com>
2019-04-12 09:39:16 +0800

    Adapter的继承实现

---
+++ DesignPattern/src/com/lqp/Adapter/extend/Main.java
@@ -0,0 +1,10 @@
+package com.lqp.Adapter.extend;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Print p=new PrintBanner("Hello");
+        p.printWeak();
+        p.printStrong();
+    }
+}
---
+++ DesignPattern/src/com/lqp/Adapter/extend/Print.java
@@ -0,0 +1,7 @@
+package com.lqp.Adapter.extend;
+
+public interface Print {
+    public abstract void printWeak();
+    public abstract void printStrong();
+
+}
